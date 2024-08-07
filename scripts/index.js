b4cc8cd5a18b77e869afa2381abfb85c7bcaa841
WhisperKnight <61612853+Bryson-C@users.noreply.github.com>
2024-08-07 12:58:55 -0600

    Add files via upload
    Added new blog "Riot Games Is Actually Really Good"

--- scripts/index.js
+++ scripts/index.js
@@ -8,10 +8,27 @@ const subHeaderTexts = [
 ]
 
 function getRandomSubHeaderText() {
-    const text = subHeaderTexts[Math.floor(Math.random() * subHeaderTexts.length)]
-    console.log(text)
-    return text
+    return subHeaderTexts[Math.floor(Math.random() * subHeaderTexts.length)]
 }
 
 // This Is Defered, This Shouldn't Fail As The Document Is Always Loaded Before The Script
 document.getElementById("sub-title").innerHTML = getRandomSubHeaderText()
+
+/*
+for (let elm of document.getElementsByClassName("blog-link")) {
+    elm.addEventListener("mouseout", function() {
+        let blog_tags = this.parentNode.getElementsByClassName("blog-tag-list")[0].children;
+        for (let tag of blog_tags) {
+            //tag.style.display = "none";
+            tag.style.opacity = 0;
+        }
+    });
+    elm.addEventListener("mouseover", function() {
+        let blog_tags = this.parentNode.getElementsByClassName("blog-tag-list")[0].children;
+        for (let tag of blog_tags) {
+            //tag.style.display = "inline";
+            tag.style.opacity = 1;
+        }
+    });
+}
+*/
