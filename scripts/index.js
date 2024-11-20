294961ab285590d60f998a0f3cc633bb80b8366c
WhisperKnight <61612853+Bryson-C@users.noreply.github.com>
2024-11-19 20:22:55 -0700

    Add files via upload

--- scripts/index.js
+++ scripts/index.js
@@ -4,6 +4,7 @@ const subHeaderTexts = [
     "Imagine If I Created Anything But Slop",
     "Java Devs Wouldn't Understand, Nor Do I Expect Them To",
     "😔",
+    "😎",
     "Local Man, Global Hater",
     "Do Your Country A Favor: Report Those Commies!",
     "Trust Me Bro, You Just Have To Get Past The First 30 Episodes",
@@ -22,15 +23,33 @@ const subHeaderTexts = [
     "Freewill? More Like Dismiss William",
     "Mods, Launch This Guy To Orbit",
     "700+ Dependencies Node/Cargo (And Counting!!!)",
-    "I Should Have Read My Textbooks Instead Of Ranting About AI!"
+    "I Should Have Read My Textbooks Instead Of Ranting About AI!",
+    "Lets Perscribe Children Drugs!!! Doctors, Cash In Those Checks!!!"
 ]
 
+function random(len) {
+    return Math.floor(Math.random() * len);
+}
+
+const enableEpicMusic = false;
+
 function getRandomSubHeaderText() {
-    return subHeaderTexts[Math.floor(Math.random() * subHeaderTexts.length)]
+    let subHeader = subHeaderTexts[random(subHeaderTexts.length)];
+    if (subHeader === "😎" && enableEpicMusic === true) {
+        let xxEpicMusicSlayerXX = new Audio("music/epic.mp3");
+        xxEpicMusicSlayerXX.volume = 0.5;
+        xxEpicMusicSlayerXX.currentTime = 22;
+        xxEpicMusicSlayerXX.play().then(()=>{
+
+        }).catch(()=> {
+            console.log("Bro didn't allow audio, your loss")
+        });
+    }
+    return subHeader;
 }
 
 // This Is Defered, This Shouldn't Fail As The Document Is Always Loaded Before The Script
-document.getElementById("sub-title").innerHTML = getRandomSubHeaderText()
+document.getElementById("sub-title-animated").innerHTML = getRandomSubHeaderText()
 
 function addTags(element, strings, color) {
     for (let i = 0; i < strings.length; i++) {
@@ -40,3 +59,39 @@ function addTags(element, strings, color) {
         element.appendChild(childTag);
     }
 }
+
+
+
+
+const imageSources = [
+    [],
+    ["blogs/Img/AsylumSolaire.png","blogs/Img/BadDog.png"],
+    ["blogs/Img/BlackGultchMyBeloved.png","blogs/Img/DS2ControllerIdea.png"],
+    ["blogs/Img/ChinaLover400xx.png","blogs/Img/RooftopGlare.png"],
+    ["blogs/Img/ReaderSpotted.png","blogs/Img/CommieBook.png"],
+    ["blogs/Img/ShutMouth.jpg","blogs/Img/Application.png","blogs/Img/FakeJobExposed.png"],
+    ["blogs/Img/AITypist.png","blogs/Img/ConspiracyTheorist.png","blogs/Img/FuckYouImTheMainCharacter.png"],
+];
+
+function setSwapingImages(id, imageArrayIndex) {
+    let elm = document.getElementById(id);
+    let imageIndex = 0;
+    if (imageSources[imageArrayIndex].length > 0) {
+        setInterval(() => {
+            elm.src = imageSources[imageArrayIndex][imageIndex];
+            imageIndex++;
+            if (imageIndex > imageSources[imageArrayIndex].length-1) imageIndex = 0;
+        }, 3000 + ((imageArrayIndex % 2) * 500));
+    }
+}
+
+setSwapingImages("blog-post-1", 0);
+setSwapingImages("blog-post-2", 1);
+setSwapingImages("blog-post-3", 2);
+setSwapingImages("blog-post-4", 3);
+setSwapingImages("blog-post-5", 4);
+setSwapingImages("blog-post-6", 5);
+setSwapingImages("blog-post-7", 6);
+
+
+
