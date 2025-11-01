624022b462fa6e00c7e10c04aac3af170c7027dd
WhisperKnight <61612853+Bryson-C@users.noreply.github.com>
2025-11-01 17:47:01 -0600

    Delete blogs directory

--- blogs/scripts/ItDoesntStop.js
+++
@@ -1,8 +0,0 @@
-function toggleNavbarOptions() {
-    let article_section = document.getElementById("it-doesnt-stop-navbar")
-    if (article_section.style.display === "none") {
-        article_section.style.display = "grid";
-    } else {
-        article_section.style.display = "none";
-    }
-}
--- blogs/scripts/academichonesty.js
+++
@@ -1,8 +0,0 @@
-function toggleBuzzFeedFreakShit() {
-    let article_section = document.getElementById("buzzfeed-freak-shit")
-    if (article_section.style.display === "none") {
-        article_section.style.display = "inline";
-    } else {
-        article_section.style.display = "none";
-    }
-}
